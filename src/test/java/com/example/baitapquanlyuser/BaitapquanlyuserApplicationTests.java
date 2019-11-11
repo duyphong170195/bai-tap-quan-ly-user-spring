39596953817d9715009a3bbb3b46bc2ac70cb7ff
luv-phongnd <phongnd@luvina.net>
2019-11-11 14:43:34 +0700

    create project spring

---
+++ src/test/java/com/example/baitapquanlyuser/BaitapquanlyuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baitapquanlyuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaitapquanlyuserApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
